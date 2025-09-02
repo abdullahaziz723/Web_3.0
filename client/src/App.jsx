39f270a6c628c8e3cf54904946bd1f5bb8173ae6
abdullah <abdullahaziz723@gmail.com>
2025-09-02 16:00:34 +0530

    web_3.0

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+
+const  App=()=> {
+
+  return (
+    <>
+      <div>
+    
+          <h1 class="  text-2xl font-bold underline">Hello world!</h1>
+       
+      </div>
+    </>
+  );
+}
+
+export default App;
